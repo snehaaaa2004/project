73c9148f5bdb66ba416f28fa26a0e0ce9a747d8a
snehaaaa2004 <snehajacob2004@gmail.com>
2025-05-02 13:32:39 +0530

    Delete frontend directory

--- frontend/src/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const getPlans = async (category, type) => {
-  console.log('Frontend sending:', category, type);
-  const res = await axios.get(`http://localhost:3000/plans?category=${category}&type=${type}`);
-  return res.data;
-};
